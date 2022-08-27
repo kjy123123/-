618415b015745f9b7624af7f96d32572b7cfc773
kjy123123 <112236122+kjy123123@users.noreply.github.com>
2022-08-27 15:09:12 +0900

    8월27일 조건문

---
+++ 김재영/조건.py
@@ -0,0 +1,17 @@
+money = 10000
+if money > 5000:
+    print("택시를 타자")
+elif money < 5000:
+    print("대중교통을 이용하자")
+
+a= ["a","c","e"]
+
+if "c" in a:
+    print("있다")
+else:
+    print("없다")
+
+
+a=int(input()) # 10 9
+print(a%2)     # 0  1  5%2=1, %:나누었을 때 나머지
+if a&2 %0
